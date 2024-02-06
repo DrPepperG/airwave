4e261df5994ff427924f91f77aab429c6b52136f
DrPepperG <josephcrain14@gmail.com>
2024-02-05 22:28:24 -0500

    store new tokens

--- routes/callback.get.ts
+++ routes/callback.get.ts
@@ -1,15 +1,37 @@
+import { createItem } from "@directus/sdk";
+
 export default eventHandler(async (event) => {
+    const { mainRedirect } = useRuntimeConfig(event);
     const client = await useOAuth();
 
     // Smart guys at intuit made us send the unparsed url string instead of an object
     const query = getRequestURL(event).search;
+    const realmId = getQuery(event).realmId as string
     client.createToken(query)
         .then((res) => {
-            console.log('The Token is  ' + JSON.stringify(res.getJson()));
+            storeToken(realmId, res.getJson())
         })
         .catch((err) => {
             console.log(err);
         });
 
-    return 'ok';
+    return sendRedirect(event, mainRedirect);
 })
+
+async function storeToken(realmId: string, token: {
+    access_token: string,
+    token_type: string,
+    x_refresh_token_expires_in: number,
+    id_token: string,
+    refresh_token: string,
+    expires_in: number 
+}) {
+    const directus = await useDirectus();
+
+    const d = new Date();
+    await directus.request(createItem('quickbooks_oauth', {
+        realm_id: realmId,
+        refresh_token: encrypt(token.refresh_token),
+        expires_at: new Date(d.getTime() + (token.x_refresh_token_expires_in * 1000)).toISOString()
+    }))
+}
