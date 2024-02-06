9364d4347396cc179f7eebb0bebcaf1b6323ca23
DrPepperG <josephcrain14@gmail.com>
2024-02-06 18:37:35 -0500

    start of webhook handling

--- routes/webhook.post.ts
+++ routes/webhook.post.ts
@@ -1,4 +1,57 @@
+import { H3Event, EventHandlerRequest } from 'h3';
 import { VerifySignature } from "../classes/VerifySignature"
+
+// #region Types
+type WebhookType =
+  | "Account"
+  | "BillPayment"
+  | "Class"
+  | "Customer"
+  | "Employee"
+  | "Estimate"
+  | "Invoice"
+  | "Item"
+  | "Payment"
+  | "Purchase"
+  | "SalesReciept"
+  | "Vendor"
+  | "Bill"
+  | "CreditMemo"
+  | "RefundReceipt"
+  | "VendorCredit"
+  | "TimeActivity"
+  | "Department"
+  | "Deposit"
+  | "JournalEntry"
+  | "PaymentMethod"
+  | "Preferences"
+  | "PurchaseOrder"
+  | "TaxAgency"
+  | "Term"
+  | "Transfer"
+  | "Budget"
+  | "Currency"
+  | "JournalCode";
+
+type OperationType = "Create" | "Update" | "Merge" | "Remove";
+
+type WebhookEntity = {
+    name: WebhookType,
+    id: string,
+    operation: OperationType,
+    lastUpdated: string,
+    deletedID?: string
+}
+
+type WebhookResponse = {
+    eventNotifications: {
+        realmId: string,
+        dataChangeEvent: {
+            entities: WebhookEntity[]
+        }
+    }[]
+}
+// #endregion
 
 export default eventHandler(async (event) => {
     const body = await readBody(event);
@@ -9,7 +62,12 @@ export default eventHandler(async (event) => {
         return setResponseStatus(event, 401);
     }
 
-    console.log(JSON.stringify(body));
+    // Do not wait for this to finish, we need to respond to the intuit server
+    handleWebhook(event);
 
-    return 'hi';
+    return setResponseStatus(event, 200);
 })
+
+function handleWebhook(event: H3Event<EventHandlerRequest>) {
+    // start
+}
