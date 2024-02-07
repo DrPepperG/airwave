7071e26e78fc341884e55f1f4ed93706e383c71b
DrPepperG <josephcrain14@gmail.com>
2024-02-07 10:02:27 -0500

    Add start of database management

--- classes/CustomerManager.ts
+++ classes/CustomerManager.ts
@@ -1,7 +1,49 @@
 import type { H3Event, EventHandlerRequest } from 'h3';
 import type { WebhookEntity } from '../routes/webhook.post';
 import { BaseManager } from "./BaseManager";
+import { createItem, updateItem } from '@directus/sdk';
 
+// #region Types
+type Customer = {
+    PrimaryEmailAddr: {
+        Address: string,
+    },
+    SyncToken: string,
+    domain: string,
+    GivenName: string,
+    DisplayName: string;
+    BillWithParent: boolean,
+    FullyQualifiedName: string,
+    CompanyName: string,
+    FamilyName: string,
+    sparse: boolean,
+    PrimaryPhone: {
+        FreeFormNumber: string,
+    },
+    Active: boolean,
+    Job: boolean,
+    BalanceWithJobs: number,
+    BillAddr: {
+        City: string,
+        Line1: string,
+        PostalCode: string,
+        Lat: string,
+        Long: string,
+        CountrySubDivisionCode: string,
+        Id: string,
+    },
+    PreferredDeliveryMethod: string,
+    Taxable: boolean,
+    PrintOnCheckName: string,
+    Balance: number,
+    Id: string,
+    MetaData: {
+        CreateTime: string,
+        LastUpdatedTime: string,
+    },
+    Notes: string
+}
+// #endregion
 
 export class CustomerManager extends BaseManager {
     constructor(event: H3Event<EventHandlerRequest>, entity: WebhookEntity, realmId: string) {
@@ -11,25 +53,51 @@ export class CustomerManager extends BaseManager {
     public async handle() {
         await this.init();
 
-        switch(this.entity.operation) {
-            case 'Update':
-                this.update();
-                break;
-        }
-    }
-
-    private async update() {
         const qbo = this.qbo;
         const entity = this.entity;
+        const realmId = this.realmId;
 
-        const customer = await new Promise((resolve, reject) => {
-            qbo.getCustomer(entity.id, (err, customer) => {
+        // We get the customer from quickbooks no matter what, webhook doesn't send anything
+        const customer = await new Promise<Customer>((resolve, reject) => {
+            qbo.getCustomer(entity.id, (err, customer: Customer) => {
                 if (err) return reject(err);
                 return resolve(customer);
             })
         }).catch(console.error);
         if (!customer) return;
 
-        console.log(customer, 'ddd')
+        switch(this.entity.operation) {
+            case 'Create':
+                await this.create(customer, realmId);
+                break;
+            case 'Update':
+                await this.update(customer, realmId);
+                break;
+        }
+    }
+
+    /**
+     * Create the customer in our database
+     */
+    private async create(customer: Customer, realmId: string) {
+        const directus = await useDirectus();
+
+        await directus.request(createItem('customers', {
+            quickbooks_id: customer.Id
+        }))
+    }
+
+    /**
+     * Update the customer in our database
+     */
+    private async update(customer: Customer, realmId: string) {
+        const directus = await useDirectus();
+
+        await directus.request(updateItem('customers', customer.Id, {
+            //
+        })).catch((reason) => {
+            if (reason.response.status !== 403) return; // We get a 403 if the object doesn't exist
+            this.create(customer, realmId);
+        })
     }
 }
